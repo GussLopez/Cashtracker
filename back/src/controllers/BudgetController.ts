00451151536919c8ccda2b16fc333587fa30f4a2
Gustavo <gustavolopez819@gmail.com>
2026-07-04 22:27:45 -0500

    feat(budget): add get all budgets endpoint

--- back/src/controllers/BudgetController.ts
+++ back/src/controllers/BudgetController.ts
@@ -3,16 +3,27 @@ import Budget from "../models/Budget";
 
 export class BudgetController {
   static getAll = async (req: Request, res: Response) => {
-    console.log("Desde budget");
+    try {
+      const budgets = await Budget.findAll({
+        order: [
+          ['craetedAt', 'DESC']
+        ],
+        //TODO: Filtrar por el user 
+      });
+
+      res.json(budgets);
+    } catch (error) {
+      res.status(500).json({ error: "There was an error" });
+    }
   };
 
   static create = async (req: Request, res: Response) => {
     try {
       const budget = new Budget(req.body);
 
-      await budget.save()
+      await budget.save();
 
-      res.status(201).json('Budget created');
+      res.status(201).json("Budget created");
     } catch (error) {
       res.status(500).json({ error: "There was an error" });
     }
