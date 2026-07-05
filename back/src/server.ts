8a14887d5fce5d93374a41bf5f5160848f14577a
Gustavo <gustavolopez819@gmail.com>
2026-07-04 21:57:18 -0500

    feat(api): create connect db function and use budget router

--- back/src/server.ts
+++ back/src/server.ts
@@ -1,10 +1,24 @@
 import express from 'express' 
 import colors from 'colors'
 import morgan from 'morgan'
+import { db } from './config/db';
+import budgetRouter from './router/budgetRouter';
 
+async function connectDb() {
+  try {
+    await db.authenticate();
+    db.sync();
+    console.log(colors.blue.bold('Conexión exitosa a la DB'));
+  } catch (error) {
+    console.log( colors.red.bold('Falló la conexión a la BD'));
+  }
+}
+connectDb();
 const app = express()
 
 app.use(morgan('dev'))
+
+app.use('/api/budgets', budgetRouter);
 
 app.use(express.json())
 
