8683559d7b0fde644532bbc8a92f3974aa9005d5
Gustavo <gustavolopez819@gmail.com>
2026-07-05 23:03:46 -0500

    refactor(budget): consolidate validation middleware for budget routes

--- back/src/router/budgetRouter.ts
+++ back/src/router/budgetRouter.ts
@@ -2,44 +2,36 @@ import { Router } from "express";
 import { BudgetController } from "../controllers/BudgetController";
 import { body, param } from "express-validator";
 import { handleInputErrors } from "../middleware/validation";
-import { validateBudgetId, validateBudgetExist } from "../middleware/budget";
+import { validateBudgetId, validateBudgetExist, validateBudgetInput } from "../middleware/budget";
+import { ExpensesController } from "../controllers/ExpenseController";
 
 const router = Router();
+
+router.param('budgetId', validateBudgetId);
+router.param('budgetId', validateBudgetExist);
 
 router.get('/', BudgetController.getAll);
 
 router.post('/',
-  body('name')
-    .notEmpty().withMessage('The budget name cant be empty'),
-  body('amount')
-    .notEmpty().withMessage('The budget amount cant be empty')
-    .isNumeric().withMessage('Invalid quantity')
-    .custom((value) => value > 0 ).withMessage('The budget must be 1 +'),
+  validateBudgetInput,
   handleInputErrors,
   BudgetController.create
 );
 
-router.get('/:id', 
-  validateBudgetId,
-  validateBudgetExist,
-  BudgetController.getById
-);
-router.put('/:id',
-  validateBudgetId,
-  validateBudgetExist,
-  body('name')
-    .notEmpty().withMessage('The budget name cant be empty'),
-  body('amount')
-    .notEmpty().withMessage('The budget amount cant be empty')
-    .isNumeric().withMessage('Invalid quantity')
-    .custom((value) => value > 0 ).withMessage('The budget must be 1 +'),
+router.get('/:budgetId', BudgetController.getById);
+router.put('/:budgetId',
+  validateBudgetInput,
   handleInputErrors,
   BudgetController.updateById
 );
 
-router.delete('/:id', 
-  validateBudgetId,
-  BudgetController.deleteById
-);
+router.delete('/:budgetId', BudgetController.deleteById);
+
+/* Route for epenses */
+router.get('/:budgetId/expenses', ExpensesController.getAll);
+router.post('/:budgetId/expenses', ExpensesController.create);
+router.get('/:budgetId/expenses/:expenseId', ExpensesController.getById);
+router.put('/:budgetId/expenses/:expenseId', ExpensesController.updateById);
+router.delete('/:budgetId/expenses/:expenseId', ExpensesController.deleteById);
 
 export default router
