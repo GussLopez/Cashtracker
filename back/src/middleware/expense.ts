a83eaefe384f12d45e43dee2dcad95b8f2e941d0
Gustavo <gustavolopez819@gmail.com>
2026-07-07 12:17:56 -0500

    feat(expense): add function to validate expense id

--- back/src/middleware/expense.ts
+++ back/src/middleware/expense.ts
@@ -1,12 +1,51 @@
 import { Request, Response, NextFunction } from "express";
-import { body } from "express-validator";
+import { body, param, validationResult } from "express-validator";
+import Expense from "../models/Expense";
+
+declare global {
+  namespace Express {
+    interface Request {
+      expense?: Expense;
+    }
+  }
+}
 
 export const validateExpenseInput = async ( req: Request, res: Response, next: NextFunction ) => {
   await body('name')
-    .notEmpty().withMessage('The budget name cant be empty').run(req)
+    .notEmpty().withMessage('The expense name cant be empty').run(req)
   await body('amount')
-    .notEmpty().withMessage('The budget amount cant be empty')
+    .notEmpty().withMessage('The expense amount cant be empty')
     .isNumeric().withMessage('Invalid quantity')
-    .custom((value) => value > 0 ).withMessage('The budget must be 1 +').run(req)
+    .custom((value) => value > 0 ).withMessage('The expense must be 1 +').run(req)
   next();
 }
+
+export const validateExpenseId = async ( req: Request, res: Response, next: NextFunction ) => {
+  await param('expenseId').isInt().custom(value => value > 0)
+    .withMessage('Invalid ID').run(req);
+    
+    let errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    
+  next();
+}
+
+export const validateExpenseExist = async ( req: Request, res: Response, next: NextFunction ) => {
+  try {
+    const { expenseId } = req.params;
+    const expense = await Expense.findByPk(Number(expenseId));
+
+   if (!expense) {
+      const error = new Error("Expense not found");
+      res.status(404).json({ error: error.message });
+      return;
+    }
+    req.expense = expense;
+
+    next();
+  } catch (error) {
+    res.status(500).json({ error: "There was an error" });
+  }
+};
