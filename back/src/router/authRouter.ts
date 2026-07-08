e29193248cb80bde8f88b4c9b95592186973fa8f
Gustavo <gustavolopez819@gmail.com>
2026-07-07 23:57:49 -0500

    feat(auth): add create account enpoint with validation

--- back/src/router/authRouter.ts
+++ back/src/router/authRouter.ts
@@ -1,8 +1,18 @@
 import { Router } from "express";
 import { AuthController } from "../controllers/AuthController";
+import { body } from "express-validator";
+import { handleInputErrors } from "../middleware/validation";
 
 const router = Router();
 
-router.post('/create-account', AuthController.createAccount);
+router.post('/create-account', 
+  body('name')
+    .notEmpty().withMessage('The name cant be empty'),
+  body('password')
+    .isLength({ min: 8 }).withMessage('The password is too short, minimum 8 characters'),
+  body('email')
+    .isEmail().withMessage('Invalid email'),
+  handleInputErrors,
+AuthController.createAccount);
 
 export default router;
