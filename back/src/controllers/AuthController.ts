49c32a5e2d535b73c378fdb8a017f53b07ddf0e7
Gustavo <gustavolopez819@gmail.com>
2026-07-08 14:32:16 -0500

    feat(login): add login method to login endpoint

--- back/src/controllers/AuthController.ts
+++ back/src/controllers/AuthController.ts
@@ -1,9 +1,11 @@
 import type { Response, Request } from "express";
 import User from "../models/User";
+import { hashPassword } from "../utils/auth";
+import { generateToken } from "../utils/token";
 
 export class AuthController {
   static createAccount = async (req: Request, res: Response) => {
-    const { email } = req.body;
+    const { email, password } = req.body;
 
     const userExists = await User.findOne({ where: { email }});
     if (userExists) {
@@ -13,11 +15,24 @@ export class AuthController {
 
     try {
       const user = new User(req.body);
-      
+      user.password = await hashPassword(password);
+      user.token = generateToken();
       await user.save();
       res.json('Account created');
     } catch (error) {
       res.status(500).json({ error: 'There was an error' });
     }
   }
+
+   static login = async (req: Request, res: Response) => {
+    const { email } = req.body;
+
+    const user = await User.findOne({ where: { email }});
+     if (!user) {
+      const error = new Error('Account not found');
+      return res.status(409).json({ error: error.message });
+    }
+
+    res.json(user)
+   }
 }
