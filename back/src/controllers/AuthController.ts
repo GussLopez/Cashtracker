77dcc7cb0cbb0b48c65423167f566944d937279b
Gustavo <gustavolopez819@gmail.com>
2026-07-07 23:59:40 -0500

    feat(auth): add create account method

--- back/src/controllers/AuthController.ts
+++ back/src/controllers/AuthController.ts
@@ -1,7 +1,15 @@
 import type { Response, Request } from "express";
+import User from "../models/User";
 
 export class AuthController {
   static createAccount = async (req: Request, res: Response) => {
-    res.json('Creando cuenta')
+    try {
+      const user = new User(req.body);
+      
+      await user.save();
+      res.json('Account created');
+    } catch (error) {
+      res.status(500).json({ error: 'There was an error' });
+    }
   }
 }
