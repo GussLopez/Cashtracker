6fd573efe24e54c4575c553cf60650b73022d05e
Gustavo <gustavolopez819@gmail.com>
2026-07-12 23:58:19 -0500

    feat(test): add unit test for BudgetController.getAll

---
+++ back/src/tests/unit/BudgetController.test.ts
@@ -0,0 +1,22 @@
+import { createRequest, createResponse } from 'node-mocks-http'
+import { budgets } from "../mocks/budgets"
+import { BudgetController } from '../../controllers/BudgetController'
+
+jest.mock('../../models/Budget', () => ({
+  findAll: jest.fn()
+}))
+
+describe('BudgetController.getAll', () => {
+  it('should retrieve 3 budgets', async () => {
+    
+    const req = createRequest({
+      method: 'GET',
+      url: '/api/budgets',
+      user: { id: 1 }
+    })
+
+    const res = createResponse();
+
+    await BudgetController.getAll(req, res);
+  })
+})
