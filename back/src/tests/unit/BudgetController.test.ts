c93697b4b7389a09ce60bfff94e5fbf5b8d1ae80
Gustavo <gustavolopez819@gmail.com>
2026-07-13 23:04:35 -0500

    feat(test): update unit test for BudgetController.getAll to reflect user ID changes

--- back/src/tests/unit/BudgetController.test.ts
+++ back/src/tests/unit/BudgetController.test.ts
@@ -1,22 +1,31 @@
 import { createRequest, createResponse } from 'node-mocks-http'
 import { budgets } from "../mocks/budgets"
 import { BudgetController } from '../../controllers/BudgetController'
+import Budget from '../../models/Budget'
 
 jest.mock('../../models/Budget', () => ({
   findAll: jest.fn()
 }))
 
 describe('BudgetController.getAll', () => {
-  it('should retrieve 3 budgets', async () => {
+  it('should retrieve 2 budgets for user with ID 1', async () => {
     
     const req = createRequest({
       method: 'GET',
       url: '/api/budgets',
-      user: { id: 1 }
+      user: { id: 2 }
     })
 
     const res = createResponse();
 
+    const updatedBudgets = budgets.filter(budget => budget.userId === req.user.id);
+    (Budget.findAll as jest.Mock).mockResolvedValue(updatedBudgets)
     await BudgetController.getAll(req, res);
+
+    const data = res._getJSONData();
+
+    expect(data).toHaveLength(1);
+    expect(res.statusCode).toBe(200);
+    expect(res.status).not.toBe(404);
   })
 })
